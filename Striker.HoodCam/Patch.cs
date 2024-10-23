662e8db18c7cdebd082c4da26a0c7e96242fe56a
andstriker <134649361+andstriker@users.noreply.github.com>
2024-10-23 16:37:06 +0200

    Add files via upload

--- Striker.HoodCam/Patch.cs
+++ Striker.HoodCam/Patch.cs
@@ -2,35 +2,59 @@
 using MelonLoader;
 using HarmonyLib;
 using UnityEngine;
-using System.Runtime.CompilerServices;
-using static MelonLoader.MelonLogger;
-using UnityEngine.SceneManagement;
-using Il2CppCinemachine;
 
 namespace Striker.HoodCamMod
 {
     public class HoodCamMod : MelonMod
     {
+        public Vector3 newLocalPosition = new(0f, 0.03f, 0.025f);
+
         public override void OnSceneWasLoaded(int buildindex, string SceneName)
         {
-            if(RCC_SceneManager.instance != null)
+            if (IsOutside())
             {
-                if (RCC_SceneManager.instance.gameObject.scene.name != "_mainMenu" &&
-                RCC_SceneManager.instance.gameObject.scene.name != "Car Garage")
-                {
-                    if (RCC_SceneManager.instance.activePlayerCamera != null)
-                    {
-                        RCC_Camera cam = RCC_SceneManager.instance.activePlayerCamera;
+                RCC_Camera cam = RCC_SceneManager.instance.activePlayerCamera;
 
-                        Vector3 newLocalPosition = new Vector3(0f, 0.03f, 0.025f);
-
-                        cam.bumperCam.transform.localPosition = newLocalPosition;
-                    }
-                }
+                cam.bumperCam.transform.localPosition = newLocalPosition;
             }
         }
 
         public override void OnUpdate()
+        {
+            if (IsOutside())
+            {
+                RCC_Camera cam = RCC_SceneManager.instance.activePlayerCamera;
+
+                float cockpitMinFOV = 50;
+                float cockpitMaxFOV = 85;
+
+                if (cam.cameraMode == RCC_Camera.CameraMode.BUMPER)
+                {
+                    cam.FPSMinFOV = 63;
+                    cam.FPSMaxFOV = 75; // this feels OK, might need tweaking
+
+                    cam.speedBasedMovement = cam.mainSpeedCurve.Evaluate(Mathf.Clamp01(cam.speed_smoothed / 500f));
+
+                    cam.wantedFOV = Mathf.Lerp(cam.FPSMinFOV, cam.FPSMaxFOV * Mathf.Lerp(0.8f, 1f, (float)cam.GodConstant.gameSettings.game_fovLevel / 5f),
+                                    Mathf.Clamp01(cam.speedBasedMovement + Mathf.Lerp(0f, 0.1f, cam.rpmBasedMovement)));
+
+                    if (cam.bumperCam != null) // unnecessary check but prevents errors when entering PA
+                    {
+                        if (cam.bumperCam.transform.localPosition != newLocalPosition)
+                        {
+                            cam.bumperCam.transform.localPosition = newLocalPosition;
+                        }
+                    }
+                }
+                else if (cam.cameraMode == RCC_Camera.CameraMode.FPS)
+                {
+                    cam.FPSMinFOV = cockpitMinFOV;
+                    cam.FPSMaxFOV = cockpitMaxFOV;
+                }
+            }
+        }
+
+        public bool IsOutside()
         {
             if (RCC_SceneManager.instance != null)
             {
@@ -39,29 +63,11 @@ namespace Striker.HoodCamMod
                 {
                     if (RCC_SceneManager.instance.activePlayerCamera != null)
                     {
-                        RCC_Camera cam = RCC_SceneManager.instance.activePlayerCamera;
-
-                        float cockpitMinFOV = 50;
-                        float cockpitMaxFOV = 85;
-
-                        if (cam.cameraMode == RCC_Camera.CameraMode.BUMPER)
-                        {
-                            cam.FPSMinFOV = 63;
-                            cam.FPSMaxFOV = 75; // check bumperCam code for affectors
-
-                            cam.speedBasedMovement = cam.mainSpeedCurve.Evaluate(Mathf.Clamp01(cam.speed_smoothed / 500f));
-
-                            cam.wantedFOV = Mathf.Lerp(cam.FPSMinFOV, cam.FPSMaxFOV * Mathf.Lerp(0.8f, 1f, (float)cam.GodConstant.gameSettings.game_fovLevel / 5f), 
-                                            Mathf.Clamp01(cam.speedBasedMovement + Mathf.Lerp(0f, 0.1f, cam.rpmBasedMovement)));
-                        }
-                        else if (cam.cameraMode == RCC_Camera.CameraMode.FPS)
-                        {
-                            cam.FPSMinFOV = cockpitMinFOV;
-                            cam.FPSMaxFOV = cockpitMaxFOV;
-                        }
+                        return true;
                     }
                 }
-            }  
+            }
+            return false;
         }
     }
 }
