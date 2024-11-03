a1a61c663386c0895ffff5ef49904f5f98308af3
andstriker <134649361+andstriker@users.noreply.github.com>
2024-11-03 13:22:43 +0100

    Delete Striker.HoodCam-ml5 directory

--- Striker.HoodCam-ml5/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using MelonLoader;
-using Striker.HoodCamMod.ml5;
-
-[assembly: MelonInfo(typeof(HoodCamMod), "HoodCam Mod", "1.0.0", "str0ika")]
-//[assembly: MelonGame("PLANET JEM SOFTWARE", "NIGHT-RUNNERS PROLOGUE")]
